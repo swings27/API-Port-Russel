7292005d7e664be766c5c51f4913ac525c42fd5c
swings27 <anna.trbd@gmail.com>
2025-11-19 14:58:56 +0100

    swagger added instead of jsdoc

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// Configuration de Swagger avec swagger-jsdoc
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0', // Utilisation d'OpenAPI 3.0
+    info: {
+      title: 'API Port Russel', // Titre de l'API
+      version: '1.0.0', // Version de l'API
+      description: 'Ceci est une API REST simple documentée avec Swagger et Express',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000', // URL de l'API
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Répertoire où Swagger va chercher les fichiers contenant des commentaires
+};
+
+// Générer la documentation Swagger
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+// Utiliser Swagger UI pour afficher la documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Utilisation des middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/** 
+* @openapi
+* /:
+*   get:
+*     description: Page d'accueil
+*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
