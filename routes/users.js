3927b497b8e658dcabf02f2100c1cc4bee7d7591
swings27 <anna.trbd@gmail.com>
2025-12-17 16:29:05 +0100

    users services ready

--- routes/users.js
+++ routes/users.js
@@ -1,103 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const service = require('../services/users');
 /**
  * @swagger
- * /users:
+ * /users/{id}:
  *   get:
- *     summary: Récupère la liste des utilisateurs
+ *     summary: Récupère un utilisateur par son id
  *     tags:
  *       - Users
- *     responses:
- *       200:
- *         description: Liste des utilisateurs
  */
-router.get('/', function(req, res, next) {
-  res.status(200).send('Voici la liste des utilisateurs');
-  next();
-});
-
-/**
- * @swagger
- * /users/{email}:
- *   get:
- *     summary: Récupère un utilisateur par email
- *     tags:
- *       - Users
- *     parameters:
- *       - in: path
- *         name: email
- *         required: true
- *         description: Email de l'utilisateur
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Utilisateur trouvé
- *       404:
- *         description: Utilisateur non trouvé
- */
-router.get('/:email', (req, res) => {
-  const email = req.params.email;
-  res.status(200).send("Voici l'utilisateur " + email);
-});
+router.get('/:id', service.getUserById);
 
 /**
  * @swagger
  * /users:
- *   post:
+ *   put:
  *     summary: Crée un nouvel utilisateur
  *     tags:
  *       - Users
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *               name:
- *                 type: string
- *     responses:
- *       201:
- *         description: Utilisateur créé
  */
-router.post('/', (req, res) => {
+router.put('/', (req, res) => {
     res.status(201).send('Nouvel utilisateur ajouté !');
 });
 
 /**
  * @swagger
- * /users/{email}:
+ * /users/{id}:
  *   put:
- *     summary: Modifie un utilisateur existant
+ *     summary: Modifier un utilisateur existant
  *     tags:
  *       - Users
- *     parameters:
- *       - in: path
- *         name: email
- *         required: true
- *         description: Email de l'utilisateur à modifier
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *     responses:
- *       200:
- *         description: Utilisateur modifié
  */
-router.put('/:email', (req, res) => {
-    res.status(200).send('Utilisateur modifié');
-});
+router.patch('/:id', service.updateUser);
 
 /**
  * @swagger
@@ -106,19 +41,7 @@ router.put('/:email', (req, res) => {
  *     summary: Supprime un utilisateur
  *     tags:
  *       - Users
- *     parameters:
- *       - in: path
- *         name: email
- *         required: true
- *         description: Email de l'utilisateur à supprimer
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Utilisateur supprimé
  */
-router.delete('/:email', (req, res) => {
-    res.send('Utilisateur supprimé');
-});
+router.delete('/:id', service.deleteUser);
 
 module.exports = router;
---
+++ services/users.js
@@ -0,0 +1,93 @@
+//Importation du modèle de donéees
+const User = require('../models/users');
+
+//Récupérer un utilisateur
+exports.getUserById = async (req, res, next) => {
+    const id = req.params.id
+
+    try {
+        let user = await User.findById(id);
+
+        if (user) {
+            return res.status(200).json(user);
+        }
+
+        return res.status(404).json('Utilisateur non trouvé');
+    } catch (error) {
+        return res.status(501).json(error);
+    }
+};
+
+//Ajouter un utilisateur
+exports.createUser = async (req, res, next) {
+    const { name, firstname, email, password } = req.body;
+
+    //Vérification des champs
+    if (!name || !firstname || !email || !password) {
+        return res.status(400).json('Tous les champs sont obligatoires')
+    }
+
+    const temp = {
+        name: name.trim(),
+        firstname: firstname.trim(),
+        email: email.toLowerCase().trim(),
+        password
+    };
+
+    try {
+        let user = await User.create(temp);
+        return res.status(201).json(user);
+    } catch (error) {
+        return res.status(501).json(error)
+    }
+};
+
+//Modifier un utilisateur
+exports.updateUser = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, firstname, email, password } = req.body;
+
+      //Vérification des champs
+    if (!name || !firstname || !email || !password) {
+        return res.status(400).json('Tous les champs sont obligatoires')
+    }
+
+    const temp = {
+        name: name.trim(),
+        firstname: firstname.trim(),
+        email: email.toLowerCase().trim(),
+        password
+    };
+
+    try {
+        let user = await User.findOne({_id : id});
+
+        if (user) {
+            Object.keys(temp).forEach((key) => {
+                if (!!temp[key]) {
+                    user[key] = temp[key];
+                }
+            });
+
+            await user.save();
+            return res.status(200).json(user);
+        }
+
+        return res.status(404).json("Utilisateur non trouvé");
+    } catch (error) {
+        return res.status(501).json(error);
+    }
+};
+
+//Supprimer un utilisateur
+exports.deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        await User.deleteOne({_id : id});
+        return res.status(204).json('Utilisateur supprimé');
+    } catch (error) {
+        return res.status(501).json(error)
+    }
+};
+
